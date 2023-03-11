f1ffae45b876166ee364b45b79c0c426f5e30232
panoshu <panos.hu@gmail.com>
2023-03-12 00:10:56 +0800

    update plot

--- src/quant/sectiona.py
+++ src/quant/sectiona.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8; py-indent-offset:4 -*-
+
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import datetime
@@ -6,17 +7,41 @@ import os.path
 import sys
 
 import backtrader as bt
+from btplotting import BacktraderPlotting
+
+# import collections
+# collections.Iterable = collections.abc.Iterable
 
 
 class TestStrategy(bt.Strategy):
+    params = (('maperiod', 15),)
+
     def log(self, txt, dt=None):
-        '''Logging function for this strategy'''
         dt = dt or self.datas[0].datetime.date(0)
         print('%s, %s' % (dt.isoformat(), txt))
 
     def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
         self.dataclose = self.datas[0].close
+
+        # To keep track of pending orders and buy price/commission
         self.order = None
+        self.buyprice = None
+        self.buycomm = None
+
+        # Add a MovingAverageSimple indicator
+        self.sma = bt.indicators.SimpleMovingAverage(
+            self.datas[0], period=self.params.maperiod
+        )
+
+        # Indicators for the plotting show
+        bt.indicators.ExponentialMovingAverage(self.datas[0], period=25)
+        bt.indicators.WeightedMovingAverage(self.datas[0], period=25, subplot=True)
+        bt.indicators.StochasticSlow(self.datas[0])
+        bt.indicators.MACDHisto(self.datas[0])
+        rsi = bt.indicators.RSI(self.datas[0])
+        bt.indicators.SmoothedMovingAverage(rsi, period=10)
+        bt.indicators.ATR(self.datas[0], plot=False)
 
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -27,9 +52,18 @@ class TestStrategy(bt.Strategy):
         # Attention: broker could reject order if not enough cash
         if order.status in [order.Completed]:
             if order.isbuy():
-                self.log('BUY EXECUTED, %.2f' % order.executed.price)
-            elif order.issell():
-                self.log('SELL EXECUTED, %.2f' % order.executed.price)
+                self.log(
+                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f'
+                    % (order.executed.price, order.executed.value, order.executed.comm)
+                )
+
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:  # Sell
+                self.log(
+                    'SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f'
+                    % (order.executed.price, order.executed.value, order.executed.comm)
+                )
 
             self.bar_executed = len(self)
 
@@ -39,7 +73,14 @@ class TestStrategy(bt.Strategy):
         # Write down: no pending order
         self.order = None
 
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' % (trade.pnl, trade.pnlcomm))
+
     def next(self):
+        # Simply log the closing price of the series from the reference
         self.log('Close, %.2f' % self.dataclose[0])
 
         # Check if an order is pending ... if yes, we cannot send a 2nd one
@@ -48,21 +89,19 @@ class TestStrategy(bt.Strategy):
 
         # Check if we are in the market
         if not self.position:
+
             # Not yet ... we MIGHT BUY if ...
-            if self.dataclose[0] < self.dataclose[-1]:
-                # current close less than previous close
+            if self.dataclose[0] > self.sma[0]:
 
-                if self.dataclose[-1] < self.dataclose[-2]:
-                    # previous close less than the previous close
+                # BUY, BUY, BUY!!! (with all possible default parameters)
+                self.log('BUY CREATE, %.2f' % self.dataclose[0])
 
-                    # BUY, BUY, BUY!!! (with default parameters)
-                    self.log('BUY CREATE, %.2f' % self.dataclose[0])
-                    # Keep track of the created order to avoid a 2nd order
-                    self.order = self.buy()
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.buy()
 
         else:
-            # Already in the market ... we might sell
-            if len(self) >= (self.bar_executed + 5):
+
+            if self.dataclose[0] < self.sma[0]:
                 # SELL, SELL, SELL!!! (with all possible default parameters)
                 self.log('SELL CREATE, %.2f' % self.dataclose[0])
 
@@ -71,27 +110,53 @@ class TestStrategy(bt.Strategy):
 
 
 if __name__ == '__main__':
-
+    # Create a cerebro entity
     cerebro = bt.Cerebro()
 
+    # Add a strategy
     cerebro.addstrategy(TestStrategy)
 
+    # Datas are in a subfolder of the samples. Need to find where the script is
+    # because it could have been called from anywhere
     modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
     datapath = os.path.join(modpath, '../../datas/backtrader/orcl-1995-2014.txt')
 
+    # Create a Data Feed
     data = bt.feeds.YahooFinanceCSVData(
         dataname=datapath,
+        # Do not pass values before this date
         fromdate=datetime.datetime(2000, 1, 1),
+        # Do not pass values before this date
         todate=datetime.datetime(2000, 12, 31),
+        # Do not pass values after this date
         reverse=False,
     )
 
+    # Add the Data Feed to Cerebro
     cerebro.adddata(data)
-    cerebro.broker.setcash(10_000_000.0)
 
-    cerebro.broker.setcommission(commission=0.0001)
+    # Set our desired cash start
+    cerebro.broker.setcash(1000.0)
 
+    # Add a FixedSize sizer according to the stake
+    cerebro.addsizer(bt.sizers.FixedSize, stake=10)
+
+    # Set the commission
+    cerebro.broker.setcommission(commission=0.0)
+
+    # Print out the starting conditions
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
+    # Run over everything
     cerebro.run()
+
+    # Print out the final result
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # Plot the result
+    # cerebro.plot()
+
+    p = BacktraderPlotting(
+        style='bar',
+    )
+    cerebro.plot(p)
